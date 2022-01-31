d034b7ccea26f81de665a77f540d5ea7c9cd13bc
John Beresford <jberesford@volcaus.com>
2022-01-30 18:20:40 -0800

    terrain debug panels done :tada: and many trees

--- src/Experience/World/Terrain.js
+++ src/Experience/World/Terrain.js
@@ -11,6 +11,12 @@ export default class Terrain {
     this.experience = new Experience();
     this.resources = this.experience.resources;
 
+    if (this.experience.debug.active) {
+      this.debug = this.experience.debug;
+
+      this.setDebug();
+    }
+
     this.setModel();
     this.setMaterials();
     this.traverseModel();
@@ -22,26 +28,14 @@ export default class Terrain {
   }
 
   setMaterials() {
-    this.grass = this.resources.items['grassMatcap'];
-    this.sand = this.resources.items['sandMatcap'];
-    this.redSand = this.resources.items['redSandMatcap'];
     this.water = this.resources.items['waterMatcap'];
-    this.pineLeaves = this.resources.items['pineLeavesMatcap'];
-    this.pineWood = this.resources.items['pineWoodMatcap'];
-    this.birchLeaves = this.resources.items['birchLeavesMatcap'];
-    this.birchWood = this.resources.items['birchWoodMatcap'];
 
-    this.grass.encoding = sRGBEncoding;
-    this.sand.encoding = sRGBEncoding;
-    this.redSand.encoding = sRGBEncoding;
     this.water.encoding = sRGBEncoding;
-    this.pineLeaves.encoding = sRGBEncoding;
-    this.pineWood.encoding = sRGBEncoding;
-    this.birchLeaves.encoding = sRGBEncoding;
-    this.birchWood.encoding = sRGBEncoding;
   }
 
   traverseModel() {
+    this.islands = [];
+
     this.model.scene.traverse((child) => {
       if (
         child instanceof Mesh &&
@@ -49,35 +43,134 @@ export default class Terrain {
           child.name
         )
       ) {
-        child.material = new MeshMatcapMaterial({
-          matcap: this.grass,
+        child.material = new MeshLambertMaterial({
+          color: '#82DD40',
         });
+
+        this.islands.push(child);
       } else if (child instanceof Mesh && child.name === 'beach') {
-        child.material = new MeshMatcapMaterial({
-          matcap: this.sand,
+        child.material = new MeshLambertMaterial({
+          color: '#F2C078',
         });
+
+        this.beach = child;
       } else if (child instanceof Mesh && child.name === 'river') {
         child.material = new MeshMatcapMaterial({
           matcap: this.water,
         });
+
+        this.river = child;
       } else if (child instanceof Mesh && child.name === 'pine_leaves') {
-        child.material = new MeshMatcapMaterial({
-          matcap: this.pineLeaves,
+        child.material = new MeshLambertMaterial({
+          color: '#111D13',
         });
+
+        this.pineLeaves = child;
       } else if (child instanceof Mesh && child.name === 'logs') {
-        child.material = new MeshMatcapMaterial({
-          matcap: this.pineWood,
+        child.material = new MeshLambertMaterial({
+          color: '#3A1E03',
         });
+
+        this.logs = child;
       } else if (child instanceof Mesh && child.name === 'round_leaves') {
         child.material = new MeshLambertMaterial({
           color: '#5CC75F',
         });
-      } else if (child instanceof Mesh && child.name === 'birch_wood') {
-        child.material = new MeshMatcapMaterial({
-          matcap: this.birchWood,
+
+        this.roundLeaves = child;
+      } else if (child instanceof Mesh && child.name === 'road') {
+        child.material = new MeshLambertMaterial({
+          color: '#777788',
         });
+
+        this.road = child;
+      } else if (child instanceof Mesh && child.name === 'loose_leaves') {
+        child.material = new MeshLambertMaterial({
+          color: '#5CC75F',
+        });
+
+        this.looseLeaves = child;
+      } else if (child instanceof Mesh && child.name === 'marshland') {
+        child.material = new MeshLambertMaterial({
+          color: '#152200',
+        });
+
+        this.marshland = child;
       }
     });
+  }
+
+  setDebug() {
+    this.debugFolder = this.debug.pane.addFolder({
+      title: 'Terrain',
+      expanded: false,
+    });
+
+    this.debugParams = {
+      beach: '#F2C078',
+      roundTrees: '#5CC75F',
+      pineTrees: '#111D13',
+      looseTrees: '#5CC75F',
+      logs: '#3A1E03',
+      road: '#777777',
+      marshland: '#152200',
+    };
+
+    this.debugFolder
+      .addInput(this.debugParams, 'roundTrees', {
+        picker: 'inline',
+      })
+      .on('change', (e) => {
+        this.roundLeaves.material.color.setStyle(e.value);
+      });
+
+    this.debugFolder
+      .addInput(this.debugParams, 'pineTrees', {
+        picker: 'inline',
+      })
+      .on('change', (e) => {
+        this.pineLeaves.material.color.setStyle(e.value);
+      });
+
+    this.debugFolder
+      .addInput(this.debugParams, 'looseTrees', {
+        picker: 'inline',
+      })
+      .on('change', (e) => {
+        this.looseLeaves.material.color.setStyle(e.value);
+      });
+
+    this.debugFolder
+      .addInput(this.debugParams, 'logs', {
+        picker: 'inline',
+      })
+      .on('change', (e) => {
+        this.logs.material.color.setStyle(e.value);
+      });
+
+    this.debugFolder
+      .addInput(this.debugParams, 'road', {
+        picker: 'inline',
+      })
+      .on('change', (e) => {
+        this.road.material.color.setStyle(e.value);
+      });
+
+    this.debugFolder
+      .addInput(this.debugParams, 'marshland', {
+        picker: 'inline',
+      })
+      .on('change', (e) => {
+        this.marshland.material.color.setStyle(e.value);
+      });
+
+    this.debugFolder
+      .addInput(this.debugParams, 'beach', {
+        picker: 'inline',
+      })
+      .on('change', (e) => {
+        this.beach.material.color.setStyle(e.value);
+      });
   }
 
   addToScene() {
