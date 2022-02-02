caac1849b078c682ead03c71807fe76d69022be8
John Beresford <jberesford@volcaus.com>
2022-02-02 15:14:52 -0800

    lightmaps done :sparkles: :tada:

--- src/Experience/sources.js
+++ src/Experience/sources.js
@@ -1,15 +1,15 @@
 /**
  * TILES
  */
-import tile11 from '../img/tiles/tl_11.jpg';
-import tile12 from '../img/tiles/tl_12.jpg';
-import tile13 from '../img/tiles/tl_13.jpg';
-import tile21 from '../img/tiles/tl_21.jpg';
-import tile22 from '../img/tiles/tl_22.jpg';
-import tile23 from '../img/tiles/tl_23.jpg';
-import tile31 from '../img/tiles/tl_31.jpg';
-import tile32 from '../img/tiles/tl_32.jpg';
-import tile33 from '../img/tiles/tl_33.jpg';
+// import tile11 from '../img/tiles/tl_11.jpg';
+// import tile12 from '../img/tiles/tl_12.jpg';
+// import tile13 from '../img/tiles/tl_13.jpg';
+// import tile21 from '../img/tiles/tl_21.jpg';
+// import tile22 from '../img/tiles/tl_22.jpg';
+// import tile23 from '../img/tiles/tl_23.jpg';
+// import tile31 from '../img/tiles/tl_31.jpg';
+// import tile32 from '../img/tiles/tl_32.jpg';
+// import tile33 from '../img/tiles/tl_33.jpg';
 
 /**
  * MODELS
@@ -19,69 +19,73 @@ import terrainModel from '../models/terrain.glb';
 /**
  * LIGHT MAPS
  */
-import islandsLightMap from '../img/lightmaps/islandsLightmap.png';
+import islandsLightMap from '../img/lightmaps/islandsLightmap.jpg';
+import detailsLightMap from '../img/lightmaps/detailsLightmap.jpg';
+import roundTreesLightMap from '../img/lightmaps/roundTreesLightmap.jpg';
+import tallTreesLightMap from '../img/lightmaps/tallTreesLightmap.jpg';
+import looseTreesLightMap from '../img/lightmaps/looseTreesLightmap.jpg';
 
 /**
  * MATCAPS
  */
-import glossMatcap from '../img/matcaps/gloss.png';
-import metalMatcap from '../img/matcaps/metal.png';
-import pineLeavesMatcap from '../img/matcaps/pineLeaves.png';
-import pineWoodMatcap from '../img/matcaps/pineWood.png';
-import birchLeavesMatcap from '../img/matcaps/birchLeaves.png';
-import birchWoodMatcap from '../img/matcaps/birchWood.jpg';
-import grassMatcap from '../img/matcaps/grass3.png';
-import sandMatcap from '../img/matcaps/sand2.png';
-import redSandMatcap from '../img/matcaps/redSand.png';
-import waterMatcap from '../img/matcaps/water.png';
+// import glossMatcap from '../img/matcaps/gloss.png';
+// import metalMatcap from '../img/matcaps/metal.png';
+// import pineLeavesMatcap from '../img/matcaps/pineLeaves.png';
+// import pineWoodMatcap from '../img/matcaps/pineWood.png';
+// import birchLeavesMatcap from '../img/matcaps/birchLeaves.png';
+// import birchWoodMatcap from '../img/matcaps/birchWood.jpg';
+// import grassMatcap from '../img/matcaps/grass3.png';
+// import sandMatcap from '../img/matcaps/sand2.png';
+// import redSandMatcap from '../img/matcaps/redSand.png';
+// import waterMatcap from '../img/matcaps/water.png';
 
 export default [
   // TILES
-  {
-    name: 'tile11',
-    type: 'texture',
-    path: tile11,
-  },
-  {
-    name: 'tile12',
-    type: 'texture',
-    path: tile12,
-  },
-  {
-    name: 'tile13',
-    type: 'texture',
-    path: tile13,
-  },
-  {
-    name: 'tile21',
-    type: 'texture',
-    path: tile21,
-  },
-  {
-    name: 'tile22',
-    type: 'texture',
-    path: tile22,
-  },
-  {
-    name: 'tile23',
-    type: 'texture',
-    path: tile23,
-  },
-  {
-    name: 'tile31',
-    type: 'texture',
-    path: tile31,
-  },
-  {
-    name: 'tile32',
-    type: 'texture',
-    path: tile32,
-  },
-  {
-    name: 'tile33',
-    type: 'texture',
-    path: tile33,
-  },
+  // {
+  //   name: 'tile11',
+  //   type: 'texture',
+  //   path: tile11,
+  // },
+  // {
+  //   name: 'tile12',
+  //   type: 'texture',
+  //   path: tile12,
+  // },
+  // {
+  //   name: 'tile13',
+  //   type: 'texture',
+  //   path: tile13,
+  // },
+  // {
+  //   name: 'tile21',
+  //   type: 'texture',
+  //   path: tile21,
+  // },
+  // {
+  //   name: 'tile22',
+  //   type: 'texture',
+  //   path: tile22,
+  // },
+  // {
+  //   name: 'tile23',
+  //   type: 'texture',
+  //   path: tile23,
+  // },
+  // {
+  //   name: 'tile31',
+  //   type: 'texture',
+  //   path: tile31,
+  // },
+  // {
+  //   name: 'tile32',
+  //   type: 'texture',
+  //   path: tile32,
+  // },
+  // {
+  //   name: 'tile33',
+  //   type: 'texture',
+  //   path: tile33,
+  // },
 
   // MODELS
   {
@@ -96,56 +100,76 @@ export default [
     type: 'texture',
     path: islandsLightMap,
   },
+  {
+    name: 'detailsLightMap',
+    type: 'texture',
+    path: detailsLightMap,
+  },
+  {
+    name: 'roundTreesLightMap',
+    type: 'texture',
+    path: roundTreesLightMap,
+  },
+  {
+    name: 'tallTreesLightMap',
+    type: 'texture',
+    path: tallTreesLightMap,
+  },
+  {
+    name: 'looseTreesLightMap',
+    type: 'texture',
+    path: looseTreesLightMap,
+  },
 
   // MATCAPS
-  {
-    name: 'glossMatcap',
-    type: 'texture',
-    path: glossMatcap,
-  },
-  {
-    name: 'metalMatcap',
-    type: 'texture',
-    path: metalMatcap,
-  },
-  {
-    name: 'grassMatcap',
-    type: 'texture',
-    path: grassMatcap,
-  },
-  {
-    name: 'sandMatcap',
-    type: 'texture',
-    path: sandMatcap,
-  },
-  {
-    name: 'redSandMatcap',
-    type: 'texture',
-    path: redSandMatcap,
-  },
-  {
-    name: 'waterMatcap',
-    type: 'texture',
-    path: waterMatcap,
-  },
-  {
-    name: 'pineLeavesMatcap',
-    type: 'texture',
-    path: pineLeavesMatcap,
-  },
-  {
-    name: 'pineWoodMatcap',
-    type: 'texture',
-    path: pineWoodMatcap,
-  },
-  {
-    name: 'birchLeavesMatcap',
-    type: 'texture',
-    path: birchLeavesMatcap,
-  },
-  {
-    name: 'birchWoodMatcap',
-    type: 'texture',
-    path: birchWoodMatcap,
-  },
+  // {
+  //   name: 'glossMatcap',
+  //   type: 'texture',
+  //   path: glossMatcap,
+  // },
+  // {
+  //   name: 'metalMatcap',
+  //   type: 'texture',
+  //   path: metalMatcap,
+  // },
+  // {
+  //   name: 'grassMatcap',
+  //   type: 'texture',
+  //   path: grassMatcap,
+  // },
+  // {
+  //   name: 'sandMatcap',
+  //   type: 'texture',
+  //   path: sandMatcap,
+  // },
+  // {
+  //   name: 'redSandMatcap',
+  //   type: 'texture',
+  //   path: redSandMatcap,
+  // },
+  // {
+  //   name: 'waterMatcap',
+  //   type: 'texture',
+  //   path: waterMatcap,
+  // },
+  // {
+  //   name: 'pineLeavesMatcap',
+  //   type: 'texture',
+  //   path: pineLeavesMatcap,
+  // },
+  // {
+  //   name: 'pineWoodMatcap',
+  //   type: 'texture',
+  //   path: pineWoodMatcap,
+  // },
+  // {
+  //   name: 'birchLeavesMatcap',
+  //   type: 'texture',
+  //   path: birchLeavesMatcap,
+  // },
+  // {
+  //   name: 'birchWoodMatcap',
+  //   type: 'texture',
+  //   path: birchWoodMatcap,
+  // },
 ];
