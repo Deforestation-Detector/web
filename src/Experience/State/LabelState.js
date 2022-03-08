7b5c2426ef0bcdd2f1545af14acdd02cf9f21517
John Beresford <jberesford@volcaus.com>
2022-03-07 21:49:01 -0800

    mobile flex bug fix :tada:

--- src/Experience/State/LabelState.js
+++ src/Experience/State/LabelState.js
@@ -118,6 +118,7 @@ export default class LabelState {
         // Update the label
         let element = this.state.cursorState.getIntersection();
         this.updateLabel(element);
+        this.state.domElements.content.scrollTop = 0;
         // Set the state
         this.state.domElements.backdrop.classList.add('in');
         this.state.domElements.infoPage.classList.add('in');
