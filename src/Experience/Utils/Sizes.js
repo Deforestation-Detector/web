468fac21ce51e88768430a7ce4719277b4797230
John Beresford <jberesford@volcaus.com>
2022-03-09 22:37:13 -0800

    dpr error fix

--- src/Experience/Utils/Sizes.js
+++ src/Experience/Utils/Sizes.js
@@ -13,7 +13,7 @@ export default class Sizes extends EventEmitter {
       window.addEventListener('resize', () => {
         this.width = window.innerWidth;
         this.height = window.innerHeight;
-        this.pixelRatio = Math.min(window.devicePixelRatio, 1);
+        this.pixelRatio = Math.min(window.devicePixelRatio, 2);
 
         this.trigger('resize');
       });
